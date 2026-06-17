b3a25838e5601dda43877a466a0da65f9bee1048
eugene <eugenehyunjin@gmail.com>
2026-06-17 17:59:17 +0900

    ga4 태그 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,9 @@
 // Third-party Imports
 import 'react-perfect-scrollbar/dist/css/styles.css'
 
+// Next Imports
+import Script from 'next/script'
+
 // Type Imports
 import type { ChildrenType } from '@core/types'
 
@@ -17,7 +20,7 @@ export const metadata = {
   description: '공동주택 DX 플랫폼 전문 기업',
 
   openGraph: {
-    title: '비케이위너',
+    title: '비케이위너㈜',
     description: '공동주택 DX 플랫폼 전문 기업',
     url: 'https://bkwinner.co.kr',
     siteName: '비케이위너',
@@ -42,12 +45,50 @@ export const metadata = {
 }
 
 const RootLayout = ({ children }: ChildrenType) => {
-  // Vars
   const direction = 'ltr'
 
   return (
     <html id='__next' lang='ko' dir={direction}>
-      <body className='flex is-full min-bs-full flex-auto flex-col'>{children}</body>
+      <head>
+        {/* Google Tag Manager */}
+        <Script id='gtm-script' strategy='afterInteractive'>
+          {`
+            (function(w,d,s,l,i){
+              w[l]=w[l]||[];
+              w[l].push({
+                'gtm.start': new Date().getTime(),
+                event:'gtm.js'
+              });
+
+              var f=d.getElementsByTagName(s)[0],
+                  j=d.createElement(s),
+                  dl=l!='dataLayer'?'&l='+l:'';
+
+              j.async=true;
+              j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
+
+              f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-KPRCD4TN');
+          `}
+        </Script>
+      </head>
+
+      <body className='flex is-full min-bs-full flex-auto flex-col'>
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src='https://www.googletagmanager.com/ns.html?id=GTM-KPRCD4TN'
+            height='0'
+            width='0'
+            style={{
+              display: 'none',
+              visibility: 'hidden'
+            }}
+          />
+        </noscript>
+
+        {children}
+      </body>
     </html>
   )
 }
